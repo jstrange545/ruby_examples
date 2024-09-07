7ef1de14c5983d0596695ea99f377de68b44bd83
Jeremy Strange <Strange12221@gmail.com>
2024-09-07 15:49:36 -0400

    person main

---
+++ person_main.rb
@@ -0,0 +1,14 @@
+#require person_revisted.rb
+j = Person.new("John")
+p = Person.new("Paul")
+g = Person.new("George")
+r = Person.new("Ringo")
+j.has_friend(p)
+j.has_friend(g)
+r.has_hobby("rings")
+Person.all_with_friends(p).each do |person|
+    puts "#{person.name} is friends with #{p.name}"
+end
+Person.all_with_hobbies("rings").each do |person|
+    puts "#{person.name} is into rings"
+end
