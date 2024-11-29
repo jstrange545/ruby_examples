62c1d034c1b1799c74de0a11b6a8382dd0b1e94c
Jeremy Strange <Strange12221@gmail.com>
2024-11-29 09:49:24 -0500

    specifing a class with in a module

---
+++ hammer_driver.rb
@@ -0,0 +1,4 @@
+require "./tools"
+# This specifies the hammer class of the tools module
+yhammer = Tools::Hammer.new
+myhammer.hit()
---
+++ tools.rb
@@ -0,0 +1,7 @@
+module Tools
+    class Hammer
+        def hit()
+            puts "Hammer time"
+        end
+    end
+end
